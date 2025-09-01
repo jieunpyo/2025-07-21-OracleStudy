2d5a58966c7cdafc403302f5f9c87cb037e72fd8
jieunpyo <jieunhye1121@naver.com>
2025-09-01 17:55:42 +0900

    1111

--- OracleDatabaseProject/src/com/sist/main/MainClass.java
+++ OracleDatabaseProject/src/com/sist/main/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Oracle 연동"); 
+		System.out.println("Oracle 연동");
 	}
 
 }
