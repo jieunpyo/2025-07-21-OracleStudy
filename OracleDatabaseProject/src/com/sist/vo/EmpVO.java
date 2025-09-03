47976fa88cc2725cdf63fe31f10de1fcc3e5d78e
jieunpyo <jieunhye1121@naver.com>
2025-09-03 16:50:32 +0900

    1111

--- OracleDatabaseProject/src/com/sist/vo/EmpVO.java
+++ OracleDatabaseProject/src/com/sist/vo/EmpVO.java
@@ -1,24 +1,24 @@
 package com.sist.vo;
 /*
- * EMPNO  NOT NULL NUMBER(4)
+ EMPNO NOT NULL NUMBER(4)
  ENAME VARCHAR2(10)
  JOB  VARCHAR2(9)
- MGR  NUMBER(4)
- HIREDATE  DATE
- SAL  NUMBER(7,2)
+ MGR   NUMBER(4)
+ HIREDATE     DATE
+ SAL   NUMBER(7,2)
  COMM  NUMBER(7,2)
- DEPTNO  NUMBER(2)
+ DEPTNO     NUMBER(2)
  
-	    오라클 데이터형
-	    ------------
-	    문자 관련
-	      CHAR / VARCHAR2 / CLOB  ==> String
-	    숫자 관련
-	      NUMBER ==> int / double 
-	    날짜 관련
-	      DATE , TIMESTAMP ==> java.util.Date
-	    => 변수명은 이미 결정되어 있다 
-	       ----- 컬럼과 동일 
+      오라클 데이터형 
+      ------------
+      문자 관련 
+        CHAR / VARCHAR2 / CLOB  ==> String
+      숫자 관련 
+        NUMBER ==> int / double 
+      날짜 관련 
+        DATE , TIMESTAMP ==> java.util.Date
+      => 변수명은 이미 결정되어 있다 
+         ----- 컬럼과 동일 
  */
 import java.util.*;
 
