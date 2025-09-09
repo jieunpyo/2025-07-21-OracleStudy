56ecb8d82a1642e67c50d64e397def8c12df8b22
jieunpyo <jieunhye1121@naver.com>
2025-09-09 17:18:32 +0900

    1111

--- OracleDatabaseBoardProject/src/com/sist/main/BoardMainFrame.java
+++ OracleDatabaseBoardProject/src/com/sist/main/BoardMainFrame.java
@@ -4,20 +4,25 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 public class BoardMainFrame extends JFrame{
-	CardLayout card=new CardLayout();
-	BoardList bList;
-	public BoardMainFrame()
-	{
-		bList=new BoardList(this);
-		setLayout(card);
-		add("list",bList);
-		setSize(640, 570);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
+    CardLayout card=new CardLayout();
+    BoardList bList;
+    BoardInsert bInsert;
+    public BoardMainFrame()
+    {
+    	bList=new BoardList(this);
+    	bInsert=new BoardInsert(this);
+    	setLayout(card);
+    	add("list",bList);
+    	add("insert",bInsert);
+    	
+    	
+    	setSize(640, 570);
+    	setVisible(true);
+    	setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new BoardMainFrame();
+        new BoardMainFrame();
 	}
 
 }
--- OracleDatabaseBoardProject/src/com/sist/vo/BoardVO.java
+++ OracleDatabaseBoardProject/src/com/sist/vo/BoardVO.java
@@ -4,7 +4,7 @@ import java.util.*;
 import lombok.Data;
 @Data
 public class BoardVO {
-	private int no,hit;
-	private String name,subject,content,pwd,dbday;
-	private Date regdate;
+   private int no,hit;
+   private String name,subject,content,pwd,dbday;
+   private Date regdate;
 }
